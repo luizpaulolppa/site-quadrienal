387d81fe8ff669cce9372c27d483fcdc734c27b5
Luiz Paulo Pilegi de Almeida <luiz.pilegi@condoconta.com.br>
2022-07-14 22:48:12 -0300

    added new components

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import {} from "./styles";
+
+const Login: React.FC = () => {
+  return <h1>HOME</h1>;
+};
+
+export default Login;
---
+++ src/styles/global.tsx
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+  
+  body, input, button {
+    font: 16px Roboto, sans-serif;
+  }
+  
+  html, body, #root {
+    margin: 0 auto;
+    height: 100%;
+    background-color: #eeeeef;
+  }
+  
+  button {
+    cursor: pointer;
+  }
+  
+  h1, h2, h3, h4, h5, p {
+    margin: 0;
+  }
+`;
